84bfbff966a8ef26200cdbd2135b98c4d39f937f
Meng Le <mengle.zhang@gmail.com>
2024-07-09 12:08:07 +0100

    updated readme and added files

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# sure-start-historical-website
-This is a repository for information obtained from the former Sure Start website
+# Sure Start website data (Northern Ireland) 
+
+Sure Start is a flagship early years programme aimed at families with children under five living in the most deprived areas of Northern Ireland. It began in 2000 and has undergone a series of expansions over time (incorporating more areas of Northern Ireland). The first of these expansions took place between 2006 and 2010, and additional wards were added to the programme based on deprivation. 
+
+In 2009, the former Sure Start website (archived [here](https://web.archive.org/web/20090101115113/http://www.surestart.gov.uk/aboutsurestart/help/contacts/northernireland/)) recorded information on all the wards eligible for Sure Start.  
+
+The purpose of this repository is to format information on Sure Start coverage in a structured way for further data linkage and research. 
+
+## Usage
+
+For those interested in the cleaned data, the table of wards covered by Sure Start is contained in the `data` folder as `national archive to nimdm wards (checked).csv`. This table contains the ward codes and names of Sure Start wards (using 1992 ward boundaries—the geographical unit used at the time). 
+
+Interactive maps of the coverage areas for each Sure Start ward are in the `docs` folder and can be viewed on the project page for this repository. 
+
+
+## Data collection
+
+The earliest data on ward coverage comes from January 2009 (located by clicking on the links at the bottom of the page [here](https://web.archive.org/web/20090101115113/http://www.surestart.gov.uk/aboutsurestart/help/contacts/northernireland/)). 
+
+Ward names were:
+
+- manually inputted into R and compared to official ward names using string matching (`notebooks/note manual ward input.R`). The official ward names used come from the Northern Ireland Multiple Deprivation Index 2010 (only used as a convenient reference for ward names). Sometimes, only part of a ward may be eligible for Sure Start.  
+
+- the results of the best matching ward names and codes are outputs in  `data/national archive to nimdm wards.csv`. These results were manually checked and amended to produce the data in `national archive to nimdm wards (checked).csv`. 
+
+As part of ongoing research, we are attempting to validate the coverage data with other sources, including information held by Sure Start centres. Ideally, we would like to distinguish which wards were in the original rollout of Sure Start (2000 - 2003) and which wards were added as part of the first expansion of the programme. 
+
+## Further information
+
+This work was supported and funded by UK Research and Innovation (ES/Z502558/1) and the UK Prevention Research Partnership Maternal and Child Health Network (MR/S037608/1). 
+
+Further information about the project is available [here](https://cascadewales.org/research/the-impact-of-sure-start-on-health-and-social-care/) and [here](https://matchnet.sphsu.gla.ac.uk/pump-priming-funding/). 
+
+I can be contacted by email at [zhangm19@cardiff.ac.uk](zhangm19@cardiff.ac.uk). 
---
+++ notebooks/note manual ward input.R
@@ -0,0 +1,335 @@
+## note: manual entry and data check for Sure Start
+## Goal: enter the Sure Start wards and compare to nearest fit then manual checks
+
+
+## Source is the 2008 NI Sure Start list (5th Nov)
+## https://webarchive.nationalarchives.gov.uk/ukgwa/20081105164100/http://www.surestart.gov.uk/aboutsurestart/help/contacts/northernireland/
+library(tidyverse)
+library(RecordLinkage)
+
+### input/ output
+
+## Ward list from here 
+wards_df <- 
+  readxl::read_xls('data/NIMDM_2010_Results_Ward_0.xls', sheet = 2)
+
+## the checking function
+## takes characters and output closest 
+check_names_char <-
+  function(checkThis, checkList, nClosestToShow = 2){
+    ## lowercase
+    lower_x = checkThis %>% tolower()
+    lower_checkList = checkList %>% tolower()
+    
+    dist = levenshteinSim(lower_x, str2 = lower_checkList)
+    data.frame(
+      checkThis = checkThis,
+      #        perfectMatch = tolower(x) %in% tolower(checkList),
+      bestMatch = checkList[which.max(dist)],
+      bestMatchId = which.max(dist),
+      bestScore = max(dist), 
+      others = checkList[(dist %>% order(decreasing = T))[2:(1 + nClosestToShow)]] %>% paste(collapse = "|") #next two best        
+    )
+    
+    
+  }
+  
+# check_names_char('dave', c(letters[1:26])) ## Don't run example
+
+
+# step 1: manual data entry -----------------------------------------------
+
+
+ss <- list()
+
+
+ss$`G-Old Community Sure Start` <-
+  c('Gortalowry', 'Oldtown', 'Killymoon', 'Ardboe')
+ss$`Dalriada Sure Start` <-
+  c(
+    'Armoy',
+    'Bushmills',
+    'Ballylough',
+    'Mosside',
+    'Moyarget',
+    'Dalriada',
+    'Kinbane',
+    'Glentaise',
+    'Dunserverick',
+    'Knocklayd',
+    'Bonamargy and Rathlin'
+  )
+
+ss$`Coleraine Sure Start` <-
+  c('Ballysally',
+    'Central',
+    'Churchlands',
+    'CrossGlebe',
+    'Knocklyn and University')
+
+ss$`Ballymena South Sure Start` <-
+  c(
+    'Ballykeel',
+    'Ballee',
+    'Moat',
+    'Harryville',
+    'Dunclag',
+    'Fairgreen',
+    'Castle Demesne',
+    'Summerfield'
+  )
+
+ss$`Abbey Sure Start` <-
+  c('Abbey',
+    'Cloughfern',
+    'Coole',
+    'Dunanney',
+    'Monkstown',
+    'Valley and Whitehouse')
+
+ss$`Horizon Sure Start` <-
+  c(
+    'Northlands',
+    'Sunnylands',
+    'Clipperstown',
+    'Love Lane',
+    'Antiville',
+    'Ballyloran and Craigyhill'
+  )
+
+
+ss$`Dungannon Sure Start` <- c(
+  'Ballysaggart',
+  'Benburb',
+  'Coalisland South',
+  'Coolhill',
+  'Drumglass',
+  'Killymeal',
+  'Moygashel',
+  'Mullaghmore',
+  'Castlecaulfield'
+)
+
+ss$`Clogher Valley Sure Start` <-
+  c('Anghnacloy',
+    'Augher',
+    'Ballygawley',
+    'Clogher',
+    'Fivemiletown')
+
+ss$`South Armagh Sure Start` <-
+  c('Bessbrook',
+    'Carnlough',
+    'Creggan',
+    'Crossmaglen',
+    'Derrymore',
+    'Newtownhamilton')
+
+ss$`Orana Sure Start` <- c(
+  'Ballybot',
+  'Daisyhill',
+  'Drumalane',
+  'Drumgullion',
+  'St. Marys',
+  'St. Patricks',
+  'Windsor Hill'
+)
+
+ss$`Blossom Sure Start` <-
+  c('Annagh',
+    'Ballybay',
+    'Ballyoran',
+    'Brownstown',
+    'Cocrain',
+    'Tavanagh')
+
+ss$`Splash Sure Start` <-
+  c('Church',
+    'Court',
+    'Drumgas',
+    'Drumgor',
+    'Drumnamoe',
+    'Taghnevan')
+
+## `Western`
+  
+ss$`Strabane Sure Start` <- c(
+  'North',
+  'South',
+  'East',
+  'West',
+  'Ballycolman',
+  'Sion Mills',
+  'Finn',
+  'Dunnamanagh',
+  'Plumbridge'
+)
+
+
+ss$`Little Hands Sure Start` <-
+  c('Creevagh', 'Springtown', 'Rosemount')
+
+ss$`Shantallow Sure Start` <-
+  c('Shantallow East', 'Shantallow West', 'Carnhill', 'Culmore Area')
+
+ss$`Cherish Sure Start` <- 
+  c(
+  'Irvinestown',
+  'Kesh',
+  'Ederney and Lack',
+  'Lisnarrick',
+  'Ballinamallard',
+  'Trillick',
+  'Devenish',
+  'Rosslea',
+  'Newtownbutler'
+)
+
+ss$`Dungiven Sure Start` <-
+  c(
+    'The Highlands',
+    'Dungiven',
+    'Feeny',
+    'Upper Glenshane',
+    'Glack',
+    'Coolessan',
+    'Greystone'
+  )
+
+ss$`Last Sure Start` <- c('Lisanelly',
+                          'Drumragh',
+                          'Killyclogher',
+                          'Camowen',
+                          'Strule',
+                          'Fintona',
+                          'Termon')
+
+ss$`Edenballymore Sure Start`<- c(
+  'Brandywell',
+  'The Diamond',
+  'Westland',
+  'Strand',
+  'Beechwood',
+  'Creggan Central',
+  'Creggan South'
+)
+
+ss$`Rainbow Sure Start` <-
+  c('Castlederg',
+    'Glenderg',
+    'Clare',
+    'Drumquin',
+    'Newtownstewart')
+
+ss$`Waterside Sure Start` <- c('Victoria', 'Ebrington', 'Clondermott', 'Enagh')
+
+
+## Eastern
+
+ss$`Glenbrook Sure Start` <-
+  c('Ardoyne', 'Cliftonville', 'Ligoniel') ## This ends in a , but no further values on webpage
+
+ss$`East Belfast Sure Start` <-
+  c('Island',
+    'The Mount',
+    'Ballymacarett Woodstock',
+    'Enler',
+    'Tullycarnet')
+
+ss$`Colin Neighbourhood Sure Start` <-
+  c('Twinbrook',
+    'Poleglass',
+    'Colin Glen',
+    'Old Warren',
+    'Kilwee',
+    'Lagmore (Derriaghy)')
+
+ss$`Lower Ards Peninsula Sure Start` <-
+  c('Scrabo',
+    'Portavogie',
+    'Kircubbin',
+    'Ballywalter',
+    'Portaferry')
+
+ss$`SMILE Sure Start` <- c(
+  'New Lodge',
+  'Waterworks',
+  'Duncairn',
+  'Mount Vernon and Shore Crescent (Castleview)',
+  'York Park (Fortwilliam)'
+)
+
+ss$`Downpatrick Sure Start` <-
+  c(
+    'Cathedral',
+    'Killough',
+    'Ballymote',
+    'Ardglass',
+    "Audley's Acre",
+    'Strangford',
+    'Quoile'
+  )
+
+ss$`South Belfast Sure Start` <-
+  c('Ballynafeigh',
+    'Shaftsbury',
+    'Botanic',
+    'Blackstaff',
+    'Upper Malone')
+
+ss$`Clan Mór Sure Start` <- c('Clonard', 'Falls')
+
+ss$`Shankill Sure Start` <-
+  c('Shankill Road',
+    'Highfield',
+    'Glencairn',
+    'Woodvale',
+    'Ballysillan',
+    'Crumlin')
+
+ss$`Beechmount Sure Start` <- c('Beechmount')
+
+ss$`Outer West Belfast Sure Start` <-
+  c('Andersonstown', 'Glencolin', 'Glen Rd', 'Ladybrook')
+
+ss$`Saol Ur Sure Start` <- c('Falls Park', 'Upper Springfield', 'Whiterock')
+
+## Those are 33 Sure start areas
+
+
+# step 2: Render as table and check ---------------------------------------
+
+
+ss_df <-
+  ss %>% map_df(.f = function(x) data.frame(inputted_ward = x), .id = 'sure start centre')
+## 186 areas
+
+
+check_ss <- 
+  ss_df$inputted_ward %>% 
+  map_df(
+    .f = check_names_char,
+    checkList = wards_df$`WARD NAME`
+  )
+
+## get LDG and name
+check_ss <-
+  check_ss %>%
+  mutate(
+    bestLGD = wards_df$`LGD NAME`[bestMatchId],
+    bestCode = wards_df$`WARD CODE`[bestMatchId]
+  )
+
+check_ss <-
+  check_ss %>%
+  rename(
+    inputted_ward = checkThis
+  )
+
+check_ss
+export_this <- ss_df %>% left_join(check_ss)
+export_this <- export_this %>% arrange(bestScore)
+
+## Step 3: export and check
+export_this %>% 
+  write_csv('data/national archive to nimdm wards.csv')
